6f85583fdce9a8e11c30e3440805fde1cbc5c580
Fede180600 <fede180600@libero.it>
2022-05-24 12:32:34 +0200

    to do star rating

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,22 +3,22 @@
         <div class="container">
             <h2>film</h2>
             <div class="row row-cols-2 row-cols-md-5 justify-content-center align-items-center">
-                <div class="card" v-for="film in resultedFilms" :key="film.id">
-                    <h3>titolo film: {{ film.title }}</h3>
-                    <p>titolo originale: {{ film.original_title}}</p>
-                    <div v-if="countryFlag.includes(film.original_language.toUpperCase())">Lingua originale: <img :src="`https://catamphetamine.gitlab.io/country-flag-icons/3x2/${film.original_language.toUpperCase()}.svg`" alt="" class="language"></div>
-                    <p v-else>Lingua originale: {{ film.original_language }}</p>
-                    <p>voto: {{ film.vote_average }}</p>
+                <div class="card" v-for="item in resultedFilms" :key="item.id">
+                    <h3>titolo film: {{ item.title }}</h3>
+                    <p>titolo originale: {{ item.original_title}}</p>
+                    <div v-if="countryFlag.includes(item.original_language.toUpperCase())">Lingua originale: <img :src="`https://catamphetamine.gitlab.io/country-flag-icons/3x2/${item.original_language.toUpperCase()}.svg`" alt="" class="language"></div>
+                    <p v-else>Lingua originale: {{ item.original_language }}</p>
+                    <p>voto: {{ item.vote_average }}</p>
                 </div>
             </div>
-            <h2>serie tv</h2>
+            <h2 class="mt-2">serie tv</h2>
             <div class="row row-cols-2 row-cols-md-5 justify-content-center align-items-center">
-                <div class="card" v-for="serie in resultedSeries" :key="serie.id">
-                    <h3>nome serie TV: {{ serie.name }}</h3>
-                    <p>nome originale: {{ serie.original_name}}</p>
-                    <div v-if="countryFlag.includes(serie.original_language.toUpperCase())">Lingua originale: <img :src="`https://catamphetamine.gitlab.io/country-flag-icons/3x2/${serie.original_language.toUpperCase()}.svg`" alt="" class="language"></div>
-                    <p v-else>Lingua originale: {{ serie.original_language }}</p>
-                    <p>voto: {{ serie.vote_average }}</p>
+                <div class="card" v-for="item in resultedSeries" :key="item.id">
+                    <h3>nome serie TV: {{ item.name }}</h3>
+                    <p>nome originale: {{ item.original_name}}</p>
+                    <div v-if="countryFlag.includes(item.original_language.toUpperCase())">Lingua originale: <img :src="`https://catamphetamine.gitlab.io/country-flag-icons/3x2/${item.original_language.toUpperCase()}.svg`" alt="" class="language"></div>
+                    <p v-else>Lingua originale: {{ item.original_language }}</p>
+                    <p>voto: {{ item.vote_average }}</p>
                 </div>
             </div>
         </div>
@@ -41,6 +41,12 @@ export default {
         let countryFlagIcons = require("country-flag-icons");
         this.countryFlag = countryFlagIcons.countries;
     },
+    methods: {
+        starRating() {
+            let voteAverage = this.item.vote_average;
+            console.log(voteAverage);
+        }
+    },
 }
 </script>
 
@@ -49,6 +55,11 @@ export default {
     height: calc(100% - 60px);
     background-color: #434343;
     overflow-y: auto;
+    .container>h2 {
+        color: white;
+        text-transform: uppercase;
+        text-align: center;
+    }
     .card {
     max-height: 400px;
     min-height: 400px;
